bcee2b47db34fdfb3fb373e8b252f1ffe0b43ef3
Begas Anna <anneta.begas@gmail.com>
2018-06-11 14:01:03 +0300

    clickable number

--- full-site/templates/contacts.php
+++ full-site/templates/contacts.php
@@ -12,7 +12,14 @@
 								<p class="contacts__info_subtitle"><?=$mes['i-mes12']?></p>
 								<ul class="contacts__info_list">
 									<li class="contacts__info_item"><i class="map-ico"></i><?=$mes['i-mes13-contact']?></li>
-									<li class="contacts__info_item"><i class="phone-ico"></i><?=$mes['tel-format']?></li>
+									<li class="contacts__info_item contacts__info_item_link">
+										<a id="ringo_link1" href="" title="call" style="color:inherit; text-decoration:none;">
+											<i class="phone-ico"></i>
+											<span id="ringo-phone1" class="ringo-phone"><?=$mes['tel-format']?></span>
+
+										</a>
+
+									</li>
 									<li class="contacts__info_item"><i class="mail-ico"></i>info@kandinsky-residence.com.ua</li>
 									<li class="contacts__info_item">Пн-пт: 10.00 - 20.00</li>
 									<li class="contacts__info_item">Сб - Вск: 10.00 - 18.00</li>
@@ -24,16 +31,29 @@
 									<li class="contacts__info_item"><i class="mail-ico"></i>marketing@saga-development.com.ua</li>
 									<li>
 										<a href="#"  class="collaboration_btn"><?=$mes['collaboration']?></a>
-									</li>					
+									</li>
 								</ul>
 							</div>
 						</div>
 						<div class="contacts__form_wrapper">
-							<?php /*** modules/inc/form/ */ FormInclude('form_index');?>		
+							<?php /*** modules/inc/form/ */ FormInclude('form_index');?>
 						</div>
-						
+
 						<?php /*** modules/inc/form/ */ FormInclude('form_rieltor');?>
 					</div>
 				</div>
 		</section>
+		<script type="text/javascript">
+
+			document.getElementById("ringo_link1").onclick = function(){
+
+					var elem1  = document.getElementById("ringo_link1");
+					var source = document.getElementById("ringo-phone1").innerHTML;
+					elem1.href = 'tel:' + source;
+
+
+				 // return false;
+		 };
+
+		</script>
 <?php	 FooterAdd();		?>
