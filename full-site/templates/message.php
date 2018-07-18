765b4ddd03d0e253f5e7ae4259f5b20b3524f59a
Begas Anna <anneta.begas@gmail.com>
2018-07-18 15:52:18 +0300

    message

--- full-site/templates/message.php
+++ full-site/templates/message.php
@@ -4,9 +4,12 @@
 		<?php HeaderInclude();?>
 		<!-- header-end-->
 		<div class="page-404__content">
+			<div class="page_hidden">
+
+			</div>
 			<div class="page-404__inner">
 				<h1 class="post__title">
-				
+
 				</h1>
 				<div class="page-404__btns">
 					<div class="page-404__btns_item first-item">
@@ -23,33 +26,75 @@
 				<h2 class="page-404__title message_title ">
 					<?=$mes['message-title']?>
 				</h2>
-				<p class="page-404__text"><?=$mes['404-mes4']?></p>
-				<div class="wave-line">
-					
+				<p class="page-404__text"><?=$mes['message-text']?></p>
+				<p class="page-404__text"><?=$mes['message-text1']?></p>
+				<div class="owl-carousel owl_custom_stying">
+					<div class="other-projects__project other-projects__project__rybalsky">
+						<a target="_blank" href="https://rybalsky.com.ua/"><img src="/img/other-projects-logos/rybalsky_logo_blue.svg" alt="RYBALSKY"></a>
+					</div>
+					<div class="other-projects__project other-projects__project__bristol">
+						<a target="_blank" href="https://bristol.house/"><img src="/img/other-projects-logos/bristol_logo.svg" alt="Bristol"></a>
+					</div>
+					<div class="other-projects__project other-projects__project__newyork">
+						<a target="_blank" href="http://new-york.com.ua/"><img src="/img/other-projects-logos/new_york_logo.svg" alt="New York"></a>
+					</div>
+					<div class="other-projects__project other-projects__project__resident">
+						<a target="_blank" href="http://resident.house/"><img src="/img/other-projects-logos/resident_logo_color.svg" alt="RESIDENT"></a>
+					</div>
+					<div class="other-projects__project other-projects__project__sanfrancisco">
+						<a target="_blank" href="http://sanfrancisco.com.ua/"><img src="/img/other-projects-logos/san_francisco_color.svg" alt="San Francisco"></a>
+					</div>
+					<div class="other-projects__project other-projects__project__einstein">
+						<a target="_blank" href="https://einstein.house/"><img src="/img/other-projects-logos/einstein-logo-blue.svg" alt="EINSTEIN"></a>
+					</div>
 				</div>
+				<div class="wave-line"></div>
 			</div>
+		</div>
+
 <style>
 .page-404__content {
-	
  	min-height: 100%;
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-    -webkit-flex-direction: column;
-    -ms-flex-direction: column;
-    flex-direction: column;
-     background:url(/img/message/message-bg.jpg) center center no-repeat; 
-    -webkit-background-size: cover;
-    background-size: cover;
-    position: relative; 
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+	-webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+   background:url('/img/message/message-bg.jpg') center center no-repeat;
+  -webkit-background-size: cover;
+  background-size: cover;
+  position: relative;
+	color: white;
+	z-index: 1;
+}
+.page_hidden{
+	position: absolute;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	/* background-color: rgba(0,0,0,0.45); */
+	background-color: rgba(255,255,255,0.5);
+
+	pointer-events: none;
+	z-index: -1;
 }
 .message_title {
-	font-size: 4.875em;
-	background: rgba(255,255,255,.4);
+	font-size:50px;
+	/* background: rgba(255,255,255,.4); */
 	color:#244C5A;
+}
+.page-404__text {
+    margin: 20px auto 10px;
+    font-size: 24px;
+    line-height: 1.5;
+    display: block;
+		color:#244C5A;
+
 }
 @media screen and (max-width: 1200px){
 	.message_title {
@@ -63,6 +108,3 @@
 }
 </style>
 <?php	 FooterAdd(array('html'=>'</div>'));		?>
-	
-	
-
